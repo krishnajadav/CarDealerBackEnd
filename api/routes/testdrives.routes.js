0a01446b1eb0064ec33644b1774b96d6bd419321
Leah Isenor <leah.isenor@dal.ca>
2022-07-12 18:08:58 -0300

    Fixed spacing

--- api/routes/testdrives.routes.js
+++ api/routes/testdrives.routes.js
@@ -1,8 +1,8 @@
 //Author: Leah Isenor
 //Routes for the test drive booking feature
 const controller = require("../controllers/testdrives.controller");
+
 module.exports = function(app) {
-    
     app.use(function(req, res, next) {
         res.header(
             "Access-Control-Allow-Headers",
@@ -10,7 +10,6 @@ module.exports = function(app) {
         );
         next();
     });
-  
     app.get("/api/testdrives/appointments/unavailable", controller.getUnavailableSlots);
     app.get("/api/testdrives/appointments/:userid", controller.getAppointmentsForUser);
     app.get("/api/testdrives/timeslots", controller.getTimeslots);
